1f0e2a81ab16f9ba122abd60f9e7afe91986e930
wwlw-rab <15725422516w@gmail.com>
2026-08-10 17:50:34 +0800

    Add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'zhangliu-shell-v2';
+const BASE_URL = new URL('./', self.location.href).pathname;
+const APP_SHELL = [
+  BASE_URL,
+  `${BASE_URL}index.html`,
+  `${BASE_URL}manifest.webmanifest`,
+  `${BASE_URL}icon-192.png`,
+  `${BASE_URL}icon-512.png`,
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(
+    keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)),
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(BASE_URL))),
+  );
+});
